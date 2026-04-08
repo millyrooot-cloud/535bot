aa011f588412da364c5d574e0ff9d2abc49d79b6
Molly <kanemo@iu.edu>
2026-04-08 19:56:48 -0400

    changes

--- app.py
+++ app.py
@@ -1308,4 +1308,7 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("FLASK_ENV") == "development"
+    app.run(debug=debug, host="0.0.0.0", port=port)
